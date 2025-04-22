b29ef4bcf5bd08a4ff3e3a0287d2795058ba214a
darylyoung18 <darylyoung18755@gmail.com>
2025-04-22 19:12:01 -0400

    work on request fix

--- app/api/[alias]/route.ts
+++ app/api/[alias]/route.ts
@@ -2,12 +2,12 @@ import { NextRequest } from "next/server";
 import getCollection, { COLLECTION_NAME } from "@/db";
 
 export async function GET(
-  request: NextRequest,
-  context: any
-): Promise<Response> {
-  const alias = context.params.alias;
+    request: NextRequest,
+    context: { params: { alias: string } }
+  ): Promise<Response> {
+    const alias = context.params.alias;
   const collection = await getCollection(COLLECTION_NAME);
-  const record = await collection.findOne({ alias });
+  const record = await collection.findOne({ alias: context.params.alias });
 
   if (!record) {
     return new Response("Alias not found", { status: 404 });
