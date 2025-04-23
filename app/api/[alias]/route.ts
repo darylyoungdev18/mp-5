5b6ee6e1ad3cc29a16ece19b7d20b70ce6d1c08c
darylyoung18 <darylyoung18755@gmail.com>
2025-04-23 19:43:43 -0400

    worked on typescript issue

--- app/api/[alias]/route.ts
+++ app/api/[alias]/route.ts
@@ -1,17 +1,11 @@
 import { NextRequest } from "next/server";
 import getCollection, { COLLECTION_NAME } from "@/db";
 
-type Params = {
-  params: {
-    alias: string;
-  };
-};
-
 export async function GET(
   request: NextRequest,
-  params: Params
+  { params }: { params: { alias: string } }
 ): Promise<Response> {
-  const alias = params.params.alias;
+  const alias = params.alias;
   const collection = await getCollection(COLLECTION_NAME);
   const record = await collection.findOne({ alias });
 
