bd9bc308c1ed7ceb0cfc767864cbb8d47ae46d57
darylyoung18 <darylyoung18755@gmail.com>
2025-04-23 19:53:05 -0400

    testing

--- app/api/[alias]/route.ts
+++ app/api/[alias]/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest } from "next/server";
 import getCollection, { COLLECTION_NAME } from "@/db";
+import type { RouteParams } from "next/dist/server/future/route-matches/route-match";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { alias: string } }
+  { params }: { params: RouteParams }
 ): Promise<Response> {
-  const alias = params.alias;
+  const alias = params.alias as string;
   const collection = await getCollection(COLLECTION_NAME);
   const record = await collection.findOne({ alias });
 
